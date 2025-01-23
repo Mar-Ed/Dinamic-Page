883f91f5fe6619a79809e9b1f153e79f658a7cd4
Mar-Ed <158010261+Mar-Ed@users.noreply.github.com>
2025-01-22 21:18:48 -0500

    ultima actua

--- app/posts/page.jsx
+++ app/posts/page.jsx
@@ -3,6 +3,7 @@ import Postcard from "../../components/Postcard";
 async function loadPosts() {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
     const data = await res.json();
+    //await new Promise((resolve) => setTimeout(resolve, 3000));
     return data;
   }
   //Renderizamooos
--- components/Postcard.jsx
+++ components/Postcard.jsx
@@ -1,12 +1,18 @@
-"use client"
-function Postcard({post}) {
+"use client";
+import Link from "next/link";
+
+function Postcard({ post }) {
   return (
     <div>
-        <h3>{post.id}. {post.title}</h3>
-        <p>{post.body}</p>  
-        <button onClick={() => alert('Hola')}>Click aquí</button>
-    </div>  
-  )
+      <Link href={`/posts/${post.id}`}>
+        <h3>
+          {post.id}. {post.title}
+        </h3>
+      </Link>
+      <p>{post.body}</p>
+      <button onClick={() => alert("Hola")}>Click aquí</button>
+    </div>
+  );
 }
 
-export default Postcard
+export default Postcard;
